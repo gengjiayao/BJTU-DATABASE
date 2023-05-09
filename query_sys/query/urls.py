7cf4310d1faace0ac2c40901c2924a6d4dadf489
gengjiayao <1098469310@qq.com>
2023-05-09 23:14:18 +0800

    Fin : red word msg in login

--- query_sys/query/urls.py
+++ query_sys/query/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.toLogin_view, )
+    path('', views.toLogin_view),
+    path('index', views.Login_view),
 ]
--- query_sys/query/views.py
+++ query_sys/query/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -6,3 +7,14 @@ from django.shortcuts import render
 
 def toLogin_view(request):
     return render(request, 'login.html')
+
+
+def Login_view(request):
+    u = request.POST.get('username', '')
+    p = request.POST.get('password', '')
+
+    if u == 'admin' and p == '123456':
+        return HttpResponse("登陆成功")
+    else:
+        context = {'msg': '用户名或密码错误'}
+        return render(request, 'login.html', context)
