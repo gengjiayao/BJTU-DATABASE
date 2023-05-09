66fc1ab5c7c8c3cfb2ebe599371e87dc0d96b72a
gengjiayao <1098469310@qq.com>
2023-05-09 18:07:40 +0800

    Fin : migrate

--- query_sys/src/settings.py
+++ query_sys/src/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "train",
+        "USER": "root",
+        "PASSWORD": "k1604gengjiayao",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
