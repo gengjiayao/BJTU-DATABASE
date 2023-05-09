7616c8b1335602b7463fb23b5b94dc9d6cac0b1a
gengjiayao <1098469310@qq.com>
2023-05-10 00:40:49 +0800

    Add Hash password

--- query_sys/query/views.py
+++ query_sys/query/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+import hashlib
 
 
 # Create your views here.
-
-
 def toLogin_view(request):
     return render(request, 'login.html')
 
@@ -13,8 +13,18 @@ def Login_view(request):
     u = request.POST.get('username', '')
     p = request.POST.get('password', '')
 
-    if u == 'admin' and p == '123456':
-        return HttpResponse("登陆成功")
-    else:
-        context = {'msg': '用户名或密码错误'}
+    hash_p = hashlib.sha256(p.encode()).hexdigest()
+
+    cursor = connection.cursor()
+    cursor.execute("select user_password from user where user_account ='{}';".format(u))
+    row = cursor.fetchone()
+    if row is None:
+        context = {'msg': '该用户不存在'}
         return render(request, 'login.html', context)
+    else:
+        db_password = row[0]
+        if db_password == hash_p:
+            return HttpResponse("登陆成功")
+        else:
+            context = {'msg': '用户名或密码错误'}
+            return render(request, 'login.html', context)
